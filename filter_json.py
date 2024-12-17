2b61c2f19dfa70ded226deebdf3a5ca20b813de1
Elena <suhova.e.m.2001@gmail.com>
2024-12-17 17:20:36 +0300

    changed function process_json_files in file filter_json.py

--- filter_json.py
+++ filter_json.py
@@ -2,7 +2,7 @@ import os
 import json
 import shutil
 import time
-
+from tqdm import tqdm
 
 
 def search_keys_in_json(json_data, search_strings):
@@ -22,14 +22,14 @@ def search_keys_in_json(json_data, search_strings):
 
 
 
-def process_json_files(source_folder, target_folder, search_strings):
+def process_json_files(source_folder, target_folder, search_strings, failed_folder):
 
     if not os.path.exists(target_folder):
         os.makedirs(target_folder)
+    if not os.path.exists(failed_folder):
+        os.makedirs(failed_folder)
 
-
-    for filename in os.listdir(source_folder):
-
+    for filename in tqdm(os.listdir(source_folder)):
         if filename.endswith('.json'):
             file_path = os.path.join(source_folder, filename)
             retries = 3
@@ -38,12 +38,12 @@ def process_json_files(source_folder, target_folder, search_strings):
                     with open(file_path, 'r', encoding='utf-8') as file:
                         json_data = json.load(file)
 
-
                         if search_keys_in_json(json_data, search_strings):
-
                             shutil.copy(file_path, os.path.join(target_folder, filename))
                             print(f"Copied file: {filename}")
-                    break
+                            break
+                        else:
+                            shutil.copy(file_path, os.path.join(failed_folder, filename))
                 except Exception as e:
                     if attempt < retries - 1:
                         print(f"Error processing file {filename}: {e}. Retrying...")
@@ -51,9 +51,11 @@ def process_json_files(source_folder, target_folder, search_strings):
                     else:
                         print(f"Failed to process file {filename} after {retries} attempts: {e}")
 
+
 if __name__ == "__main__":
     source_folder = "<YOUR_PATH>"
     target_folder = "<YOUR_PATH>" # Replace with the path to your target folder
+    failed_folder = "<YOUR_PATH>" # Replace with the path to your failed_folder
     search_strings = ['Предисловие', 'Предисловие']  # Replace with the list of strings to search for in keys
 
-    process_json_files(source_folder, target_folder, search_strings)
+    process_json_files(source_folder, target_folder, search_strings, failed_folder)
